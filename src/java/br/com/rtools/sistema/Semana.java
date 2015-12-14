0451f851e86b64107b8086f10ce8513f4c19beb9
rtools2 <rtools2@producao5-pc>
2015-12-14 14:08:34 -0200

    Retorna o dia da semana conforme sis_semana (getCurrentDay()+1)

--- src/java/br/com/rtools/sistema/Semana.java
+++ src/java/br/com/rtools/sistema/Semana.java
@@ -1,5 +1,6 @@
 package br.com.rtools.sistema;
 
+import java.util.Date;
 import javax.persistence.*;
 
 @Entity
@@ -10,7 +11,7 @@ public class Semana implements java.io.Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Integer id;
     @Column(name = "ds_descricao", length = 15, unique = true)
     private String descricao;
 
@@ -19,16 +20,16 @@ public class Semana implements java.io.Serializable {
         this.descricao = "";
     }
 
-    public Semana(int id, String descricao) {
+    public Semana(Integer id, String descricao) {
         this.id = id;
         this.descricao = descricao;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -39,4 +40,9 @@ public class Semana implements java.io.Serializable {
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+
+    public Integer getCurrentDay() {
+        return new Date().getDay() + 1;
+    }
+
 }
